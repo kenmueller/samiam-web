86d3db6bcb9ddccc048ee0a3f380e22c394f6bdd
Ken Mueller <kenmueller0@gmail.com>
2023-06-12 13:56:31 -0700

    node parents

---
+++ lib/network/createBeliefNetwork.ts
@@ -0,0 +1,9 @@
+import BeliefNetwork from 'samiam/lib/belief-network'
+
+import Network from '.'
+
+const createBeliefNetwork = (network: Network) => {
+	const beliefNetwork = new BeliefNetwork()
+
+	fo
+}
