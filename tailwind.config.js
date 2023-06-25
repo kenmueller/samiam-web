574cf6612bd2a1e21566872b8ef7e8ea05b9ce98
Ken Mueller <kenmueller0@gmail.com>
2023-06-24 20:43:52 -0700

    dropdown menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: ['class'],
 	content: [
-		'./app/**/*.{js,ts,jsx,tsx,mdx,css,scss}',
-		'./components/**/*.{js,ts,jsx,tsx,mdx,css,scss}'
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}'
 	],
 	theme: {
-		screens: {},
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
 		extend: {
 			colors: {
 				charcoal: {
@@ -60,8 +69,22 @@ module.exports = {
 				raspberry: {
 					DEFAULT: '#AF3B6E'
 				}
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: 0 }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out'
 			}
 		}
 	},
-	plugins: []
+	plugins: [require('tailwindcss-animate')]
 }
