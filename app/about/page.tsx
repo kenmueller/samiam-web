0bced235ff33e428cf8cf8ad33cdd98976288b1b
Ken Mueller <kenmueller0@gmail.com>
2023-11-10 22:02:11 -0800

    about

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from 'react'
+
+import pageMetadata from '@/lib/metadata/page'
+
+export const generateMetadata = () =>
+	pageMetadata({
+		title: 'About | SamIam',
+		description: 'About SamIam',
+		previewTitle: 'About'
+	})
+
+const ArchivedNetworks = () => (
+	<div className="flex flex-col items-stretch gap-2 px-8 py-6">
+		<h1>About SamIam</h1>
+		<p>
+			Contact <a href="https://github.com/kenmueller">Ken Mueller</a>
+			for more information
+		</p>
+	</div>
+)
+
+export default ArchivedNetworks
